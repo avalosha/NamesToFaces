bbb7c8494bf4879cb0442da570e80993249b1ec2
Álvaro Ávalos Hernández <avalosha0805@gmail.com>
2020-01-14 20:52:02 -0600

    Add Xcode project. Design UICollectionView cells.

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Álvaro Ávalos Hernández on 14/01/20.
+//  Copyright © 2020 Álvaro Ávalos Hernández. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Álvaro Ávalos Hernández on 14/01/20.
+//  Copyright © 2020 Álvaro Ávalos Hernández. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
